6114a0fdd88fa5304f3461b45144e3e0538f5533
Oleksii Bashkirov <bashkirov.o.u@ukr.net>
2024-07-11 12:23:08 +0300

    Improved project structure and functionality for online-book-store. Refactored packages: - config - controller - dto - exception - mapper - model - repository - service
    Edited:
    - BookService interface: updated methods save, findById and findAll.
    - BookServiceImpl class: implemented methods for the updated interface.
    - BookRepository interface: added Optional return type to the findById method.
    - BookRepositoryImpl class: updated methods using jakarta.persistence library(EntityManagerFactory, EntityManager, EntityTransaction).
    - Application class: removed CommandLineRunner bean.
    - pom.xml file: added dependencies for mapstruct, mapstruct-processor,lombok-mapstruct-binding,mysql-connector-j and lombok. Configured maven-compiler-plugin.
    
    Created:
    - BookController class with @RestController, @RequestMapping(value = "/api/books"), and methods getAll, getBookById and createBook.
    - BookDto class with fields: id, title, author, isbn, price, description and coverImage.
    - CreateBookRequestDto class with the same fields.
    - BookMapper interface with @Mapper(config = MapperConfig.class) annotation.
    - EntityNotFoundException class.
    - MapperConfig Class with @org.mapconstruct.MapperConfig annotation.

--- src/main/java/mate/academy/repository/impl/BookRepositoryImpl.java
+++ src/main/java/mate/academy/repository/impl/BookRepositoryImpl.java
@@ -1,50 +1,51 @@
 package mate.academy.repository.impl;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import mate.academy.model.Book;
 import mate.academy.repository.BookRepository;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+@RequiredArgsConstructor
 @Repository
 public class BookRepositoryImpl implements BookRepository {
-    private final SessionFactory sessionFactory;
-
-    @Autowired
-    public BookRepositoryImpl(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
+    private final EntityManagerFactory emf;
 
     @Override
     public Book save(Book book) {
-        Session session = null;
-        Transaction transaction = null;
-        try {
-            session = sessionFactory.openSession();
-            transaction = session.beginTransaction();
-            session.persist(book);
-            transaction.commit();
+        EntityTransaction tx = null;
+        try (EntityManager em = emf.createEntityManager()) {
+            tx = em.getTransaction();
+            tx.begin();
+            em.persist(book);
+            tx.commit();
             return book;
         } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
             }
             throw new RuntimeException("Unable to save a book: " + book, e);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
+        }
+    }
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        try (EntityManager em = emf.createEntityManager()) {
+            var book = em.find(Book.class, id);
+            return Optional.ofNullable(book);
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to find book with id: " + id, e);
         }
     }
 
     @Override
     public List<Book> findAll() {
-        try (var session = sessionFactory.openSession()) {
-            var allBooksQuery = session.createQuery("SELECT b FROM Book b", Book.class);
-            return allBooksQuery.getResultList();
+        try (EntityManager em = emf.createEntityManager()) {
+            return em.createQuery("SELECT b FROM Book b", Book.class).getResultList();
         } catch (Exception e) {
             throw new RuntimeException("Unable to find all books from DB.", e);
         }
--- src/main/java/mate/academy/service/impl/BookServiceImpl.java
+++ src/main/java/mate/academy/service/impl/BookServiceImpl.java
@@ -1,28 +1,41 @@
 package mate.academy.service.impl;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import mate.academy.dto.BookDto;
+import mate.academy.dto.CreateBookRequestDto;
+import mate.academy.exception.EntityNotFoundException;
+import mate.academy.mapper.BookMapper;
 import mate.academy.model.Book;
 import mate.academy.repository.BookRepository;
 import mate.academy.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class BookServiceImpl implements BookService {
-    private BookRepository bookRepository;
+    private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
-    @Autowired
-    public BookServiceImpl(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
+    @Override
+    public BookDto save(CreateBookRequestDto requestDto) {
+        Book book = bookMapper.toModel(requestDto);
+        return bookMapper.toDto(bookRepository.save(book));
     }
 
     @Override
-    public Book save(Book book) {
-        return bookRepository.save(book);
+    public List<BookDto> findAll() {
+        return bookRepository.findAll().stream()
+                .map(bookMapper::toDto)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public List<Book> findAll() {
-        return bookRepository.findAll();
+    public BookDto findById(Long id) {
+        Book book = bookRepository.findById(id).orElseThrow(
+                        () -> new EntityNotFoundException("Book not found by id: " + id)
+                );
+        return bookMapper.toDto(book);
     }
 }
